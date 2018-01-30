f26958474b7ae1bdce4eb061d2adb9e6a45053e4
florlafuente <lafuente.florencia@gmail.com>
2018-01-30 14:51:47 -0300

    Add post params errors

--- main/errors.js
+++ main/errors.js
@@ -27,7 +27,19 @@ const ErrNotFound = new APIError('not found', {
   status: NOT_FOUND
 })
 
+const ErrInvalidParams = new APIError('Invalid paramethers', {
+  translation_key: 'INVALID_PARAMS',
+  status: 400
+})
+
+const ErrParamsTooLong = new APIError('Paramethers are too long', {
+  translation_key: 'PARAMS_LENGTH',
+  status: 400
+})
+
 module.exports = {
   APIError,
-  ErrNotFound
+  ErrNotFound,
+  ErrInvalidParams,
+  ErrParamsTooLong
 }
